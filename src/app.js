b24581eaf62d77a8503397c4ce9d859689f484f0
Anand T P <anandtp00@gmail.com>
2021-09-16 13:08:07 +0530

    bug fixes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const helmet = require('helmet')
-const expsession = require('express-session')
 
 global.__basedir1 = __dirname + "/..";
 global.__basedir = __dirname;
@@ -14,12 +13,6 @@ app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(expsession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
 
 mongoose(app);
 service(app);
